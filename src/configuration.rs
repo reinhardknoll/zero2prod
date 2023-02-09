d1dcf86ff233add6dc244b4189aeb28cb2a46c47
Reinhard <reinhard.knoll@gmail.com>
2023-02-10 00:26:48 +1300

    Trying to fix CI

--- src/configuration.rs
+++ src/configuration.rs
@@ -65,6 +65,13 @@ pub fn get_configuration() -> Result<Settings, ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(&environment_filename),
         ))
+        // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
     // Try to convert the configuration values it read into our settings type
     settings.try_deserialize::<Settings>()
